abe7e4012a88c8a9c8346cc1be6a2324fd103a6d
Ankush Ransiwal <30548906+ankushransiwal@users.noreply.github.com>
2020-04-14 23:46:22 +0530

    Added html email template

--- coronavirus.py
+++ coronavirus.py
@@ -53,7 +53,7 @@ def send_mail(country_element, total_cases, new_cases, total_deaths, new_deaths,
     msg = EmailMessage()
     msg['Subject'] = 'Coronavirus stats in your country today!'
     msg['From'] = 'ankushransiwal.13@gmail.com'
-    msg['To'] = 'ankushransiwal.13@gmail.com'
+    msg['To'] = 'abhirocz345@gmail.com'
     msg.set_content('Today in ' + country_element + '\
         \nThere is new data on coronavirus:\
         \nTotal cases: ' + total_cases +'\
@@ -64,26 +64,24 @@ def send_mail(country_element, total_cases, new_cases, total_deaths, new_deaths,
         \nTotal recovered: ' + total_recovered + '\
         \nSerious, critical cases: ' + serious_critical  + '\
         \nCheck the link: https://www.worldometers.info/coronavirus/')
-    
-    html = """\
-        <!DOCTYPE html>
-        <html> 
-        <body> 
-        <p>Hi,
-        <br> Today in  {country_element} \
-        \nThere is new data on coronavirus:\
-        \nTotal cases: {total_cases} \
-        \nNew cases: {new_cases} \
-        \nTotal deaths: {total_deaths} \
-        \nNew deaths: ' + new_deaths + '\
-        \nActive cases: ' + active_cases + '\
-        \nTotal recovered: ' + total_recovered + '\
-        \nSerious, critical cases: ' + serious_critical  + '\
-        \nCheck the link: https://www.worldometers.info/coronavirus/</p> 
-        <p><a href="https://blog.mailtrap.io/2018/09/27/cloud-or-local-smtp-server">SMTP Server for Testing: Cloud-based or Local?</a></p> <p> Feel free to <strong>let us</strong> know what content would be useful for you!</p> </body> </html>
-        """.format(**locals())
-    
-    msg.add_alternative(html, subtype='html')
+    html1= """\
+    <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office"> 
+    <head> <title></title> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><style type="text/css"> /* ----- Custom Font Import ----- */ @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700;800&family=Raleway:wght@300;800;900&display=swap');
+    body{ margin:0; padding:0; -webkit-text-size-adjust:100%; -ms-text-size-adjust:100%; -webkit-font-smoothing: antialiased; -moz-font-smoothing: antialiased; font-smoothing: antialiased;}.main{font-family: 'Poppins', sans-serif; font-weight: 300; display: inline-flex; color: #3D2D4B; font-size: 13px;}.container-inner{width: 75%;}.icon{width: 18px; height: 18px;}.daily{font-family: 'Raleway', sans-serif; font-weight: 300; display: inline-flex; font-size: 12px;}.right{text-align: right;}@media only screen and (max-width: 800px){/* ----- Base styles ----- */ .container-full{width: 80% !important;}.container-inner{width: 80%;}}@media only screen and (max-width: 600px){/* ----- Base styles ----- */ .container-full{width: 85% !important;}.container-inner{width: 90%;}.icon{width: 14px; height: 14px;}.daily{font-size: 10px;}.main{font-size: 11px;}}@media only screen and (max-width: 450px){/* ----- Base styles ----- */ .icon{width: 12px; height: 12px;}.daily{font-size: 8px;}.main{font-size: 9px;}}</style> </head> <body style="background-color:#F8D4D4;"> 
+    <div class="container-full" style="background-color: #FCFCFC; width: 700px; margin: auto;"> <div class="container-inner" style=" margin: auto; padding-top: 15px; padding-bottom: 15px;"> <div class="row"> <div class="col daily" style="width: 49%;"><img class="icon" src="https://lh3.googleusercontent.com/v0pU-8oqObn3XSR8NSFqaNbvQ-23hnUiCA79OqYyHwgmEYYaOi6j-S40vIwMktd0-CvYIYlkq476tJt6DMmjomugfwJZpmhTcJlIx5Bg7A20Y-pbyLHYtXq7nK0eSLgpppQMKuL-8Q=s50-p-k" style="display: block;"> <span style="display: block; padding-left: .5vw; padding-top:.2vw"> COVID-19 Daily Updates</span> </div><div class="col daily right" style="text-align: right; width: 49%; float: right;"> <span style="padding-top:.2vw; float: right;"> Wednesday, April 15, 2020</span> </div>
+    </div><div class="main" style="width: 100%;">
+        """
+
+    html2 = """\    
+    <p>Hi, <br>Today in {country_element} <br> There is new data on coronavirus:<br>  
+    Total cases:{total_cases} <br> New cases:{new_cases} <br>  
+    Total deaths:{total_deaths} <br> New deaths:{new_deaths} <br>  
+    Active cases: {active_cases} <br> Total recovered: {total_recovered} <br>  
+    Serious, critical cases: {serious_critical} <br>  
+    Check the link: https://www.worldometers.info/coronavirus/</p></div></div></div></body></html>
+    """.format(**locals())
+
+    msg.add_alternative((html1+html2), subtype='html')
     
     server.send_message(msg)
     print('Hey Email has been sent!')
